b2d087520a789e8aaebc783a72a559abbd58267a
PriceNotNull <pepestory2015@gmail.com>
2025-06-12 12:02:07 -0400

    Subir imágenes

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "=== Creando enlace simbólico ==="
-php artisan storage:link
+echo "🚀🚀🚀🚀 Ejecutando start.sh..🚀🚀🚀🚀"
+
+php artisan storage:link ||true
 php artisan config:cache
 php artisan serve --host=0.0.0.0 --port=8000
